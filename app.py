ceed9dcbac46e94ea73601701a727bc51b692c3e
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-12-02 11:21:17 +0100

    cwiczenie 53

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return '<h1>Strona główna</h1>'
+
+@app.route('/show_products')
+def show_products():
+    return "<h1>Lista produktów</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1>O programie</h1>"
 
 @app.route('/tests')
 def tests():
-    return "<h1>Strona testowa - zmieniona SIEMA TU MAPET!</h1>"
+    return render_template("tests.html")  #"<h1>Strona testowa - zmieniona SIEMA TU MAPET!</h1>"
 
 
 
@@ -20,3 +28,5 @@ if __name__ == '__main__':
 #Zmien nazwę funkcji obslugujacej stronę główną na index(). Każda z funkcji
 #powinna użytkownikowi wyświetlić jakiś tekst informujacy gdzie jest.
 #Włącz tryb debug dla swojej aplikacji
+
+#53. Zadbaj o to by każdy z ekranów pokazywał swój plik html z odpowiednim nagłówkiem
